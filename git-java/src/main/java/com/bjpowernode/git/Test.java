18708b949bddb90a3de90e1e0dadd484982e6f42
dong <602144047@pp.com>
2020-12-19 20:37:24 +0800

    提交整个项目

---
+++ git-java/src/main/java/com/bjpowernode/git/Test.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.git;
+
+/**
+ * wy
+ * 2020/12/19
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+
+
+}
---
+++ git/src/main/java/com/bjpd/Test.java
@@ -0,0 +1,11 @@
+package com.bjpd;
+
+/**
+ * wy
+ * 2020/12/19
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("===hello web====");
+    }
+}
