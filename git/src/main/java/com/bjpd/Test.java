fd3ccf919e49d2278572c1f85e0bfba7f7d6baab
dong <602144047@pp.com>
2020-12-21 16:16:31 +0800

    提交的更新项目

--- git/src/main/java/com/bjpd/Test.java
+++ git/src/main/java/com/bjpd/Test.java
@@ -4,6 +4,7 @@ package com.bjpd;
  * wy
  * 2020/12/19
  */
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("===hello web====");
---
+++ git/src/main/java/com/bjpd/Test2.java
@@ -0,0 +1,13 @@
+package com.bjpd;
+
+import java.sql.SQLOutput;
+
+/**
+ * wy
+ * 2020/12/21
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
